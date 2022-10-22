0e85c7faf94284f3b30635849182576c0e137220
daniel jermaine <danieljermaine2@gmail.com>
2022-10-22 20:50:31 +0100

    Initial Commit

---
+++ MapAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapAPP
+//
+//  Created by Apple on 22/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapAPP/MapAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAPPApp.swift
+//  MapAPP
+//
+//  Created by Apple on 22/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MapAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
